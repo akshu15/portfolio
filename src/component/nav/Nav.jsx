8da76533df5ea2b58ee73bfdfe625a5dc289a0f7
Akshata <akshata.as1598@gmail.com>
2022-10-22 01:51:41 -0400

    feat: Add Nav

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeHeart} from 'react-icons/bi'
+import {FaUser} from 'react-icons/fa'
+import {MdContactMail} from 'react-icons/md'
+import {GiSpellBook} from 'react-icons/gi'
+import {RiServiceLine} from 'react-icons/ri'
+import { useState } from 'react'
+
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className={ activeNav === '#' ? 'active' : ''}><BiHomeHeart/></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={ activeNav === '#about' ? 'active' : ''}><FaUser/></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={ activeNav === '#experience' ? 'active' : ''}><GiSpellBook/></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={ activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={ activeNav === '#contact' ? 'active' : ''}><MdContactMail/></a>
+
+    </nav>
   )
 }
 
