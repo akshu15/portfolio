d414c49528f49bd0937a6f1ac177109335353df8
Akshata <akshata.as1598@gmail.com>
2022-10-22 14:41:37 -0400

    feat: Add experience component

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -1,9 +1,121 @@
 import React from 'react'
 import './experience.css'
+import {AiFillHtml5} from 'react-icons/ai'
+import {DiCss3, DiJavascript1} from 'react-icons/di'
+import {FaBootstrap, FaReact, FaPython,FaNodeJs} from 'react-icons/fa'
+import {SiTailwindcss, SiPhp, SiMongodb} from 'react-icons/si'
+import {GrMysql} from 'react-icons/gr'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      {/* FRONTEND */}
+
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <AiFillHtml5 className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <DiCss3 className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <DiJavascript1 className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <FaBootstrap className='experience__details-icon'/>
+             <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+             </div>
+            </article>
+
+            <article className='experience__details'>
+              <SiTailwindcss className='experience__details-icon'/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <FaReact className='experience__details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* BACKEND */}
+
+        <div className='experience__backend'>
+          <h3>Backend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <FaNodeJs className='experience__details-icon'/>
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <SiMongodb className='experience__details-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <SiPhp className='experience__details-icon'/>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <GrMysql className='experience__details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <FaPython className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
