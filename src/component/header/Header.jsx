b411bbb3338d6599e49597f4098e74f73c3a0e92
Akshata <akshata.as1598@gmail.com>
2022-10-22 00:56:51 -0400

    feat: Add the header component part

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import me from '../../assets/akshata.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Akshata Singh</h1>
+        <h5 className='text-light'>Software Engineer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className='me'>
+          <img src={me} alt="me"/>
+        </div>
+
+          <a href='#contact' className='scroll__down'>Scroll Down</a>
+
+
+      </div>
+    </header>
   )
 }
 
